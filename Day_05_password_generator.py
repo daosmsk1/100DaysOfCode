9f24f6a81b01449115a4c8497e8a81d0bf842011
daosmsk1 <egkorin@gmail.com>
2021-11-11 20:45:09 +0300

    сделано задание дня 5

---
+++ Day_05_password_generator.py
@@ -0,0 +1,72 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+password_index_list = []
+for i in range(0, nr_letters):
+    letter_index = random.randint(0, (len(letters)-1))
+    password_index_list.append(letters[letter_index])
+for i in range(0, nr_symbols):
+    symbols_index = random.randint(0, (len(symbols)-1))
+    password_index_list.append(symbols[symbols_index])
+for i in range(0, nr_numbers):
+    numbers_index = random.randint(0, (len(numbers)-1))
+    password_index_list.append(numbers[numbers_index])
+# print(password_index_list)
+random.shuffle(password_index_list)
+password = "".join(password_index_list)
+print(f"Your password is: {password}")
+
+
+#Eazy Level
+# password = ""
+
+# for char in range(1, nr_letters + 1):
+#   password += random.choice(letters)
+
+# for char in range(1, nr_symbols + 1):
+#   password += random.choice(symbols)
+
+# for char in range(1, nr_numbers + 1):
+#   password += random.choice(numbers)
+
+# print(password)
+
+#Hard Level
+# password_list = []
+#
+# for char in range(1, nr_letters + 1):
+#   password_list.append(random.choice(letters))
+#
+# for char in range(1, nr_symbols + 1):
+#   password_list += random.choice(symbols)
+#
+# for char in range(1, nr_numbers + 1):
+#   password_list += random.choice(numbers)
+#
+# print(password_list)
+# random.shuffle(password_list)
+# print(password_list)
+#
+# password = ""
+# for char in password_list:
+#   password += char
+#
+# print(f"Your password is: {password}")
+
+
+
