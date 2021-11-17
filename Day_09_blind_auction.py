88cb4aef1582ebfd497550b8035b0f1b2438b48d
daosmsk1 <egkorin@gmail.com>
2021-11-17 20:24:32 +0300

    сделаны задания 9 дня

---
+++ Day_09_blind_auction.py
@@ -0,0 +1,63 @@
+from Day_09_art import logo
+import os
+
+auction_dict = {}
+action_goes = True
+
+print(logo)
+while action_goes:
+    name = input("What is your name?: ")
+    bid = int(input("What is your bid? $: "))
+    auction_dict[name] = bid
+    new_participant = input("Another bid (yes/no)?: ")
+    if new_participant != "yes":
+        action_goes = False
+    if new_participant == "yes":
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+winner = max(auction_dict, key=auction_dict.get)
+print(f"The winner is {winner}!")
+
+
+##########################################################################################
+
+# from art import logo
+#
+# print(logo)
+#
+# bids = {}
+# bidding_finished = False
+#
+#
+# def find_highest_bidder(bidding_record):
+#     highest_bid = 0
+#     winner = ""
+#     # bidding_record = {"Angela": 123, "James": 321}
+#     for bidder in bidding_record:
+#         bid_amount = bidding_record[bidder]
+#         if bid_amount > highest_bid:
+#             highest_bid = bid_amount
+#             winner = bidder
+#     print(f"The winner is {winner} with a bid of ${highest_bid}")
+#
+#
+# while not bidding_finished:
+#     name = input("What is your name?: ")
+#     price = int(input("What is your bid?: $"))
+#     bids[name] = price
+#     should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+#     if should_continue == "no":
+#         bidding_finished = True
+#         find_highest_bidder(bids)
+#     elif should_continue == "yes":
+#         clear()
+#
+# """
+# FAQ: My console doesn't clear()
+#
+# This will happen if you’re using an IDE other than replit.
+# I’ll cover how to use PyCharm in Day 15. That said, you can write your own clear() function or configure your IDE like so:
+#
+# https://www.udemy.com/course/100-days-of-code/learn/lecture/19279420#questions/16084076
+#
+# """
